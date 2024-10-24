5e5777c6295c37c0663c182b34fc3d7f7c425d9b
Zia-Mahmood <ziamh2003@gmail.com>
2024-10-24 19:00:23 +0530

    User Model, controllers and routes for it

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ const express = require("express");
 const session = require('express-session')
 const mongoose = require('mongoose')
 const MongoDBStore = require('connect-mongodb-session')(session)
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
+const userRoutes = require('./routes/userRoutes')
 
 
 const app = express();
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 6001
 dotenv.config({ path: './config.env' });
 
 
-mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("Connection to MongoDB successfull...")).catch((err) => console.log("Unable to connect to MongoDB...", err));
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING).then(() => console.log("Connection to MongoDB successfull...")).catch((err) => console.log("Unable to connect to MongoDB...", err));
 
 
 const mongoDBstore = new MongoDBStore({
@@ -22,22 +22,29 @@ const mongoDBstore = new MongoDBStore({
     collection: 'localSessions',
 })
 
+// MIDDLEWARES
+app.use(cors({ credentials: true, origin: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // SESSIONS HANDLER
-const MAX_AGE = 1000 * 60 * 60 * 3 // 3hrs
+const MAX_AGE = 1000 * 60 * 30; // 30 minutes
 app.use(
     session({
         secret: 'DUB_NATION',
-        name: 'session-id', // cookies name to be put in "key" field in postman
+        name: 'session-id', 
         store: mongoDBstore,
         cookie: {
-            maxAge: MAX_AGE, // this is when our cookies will expired and the session will not be valid anymore (user will be log out)
-            secure: false, // to turn on just in production
+            maxAge: MAX_AGE, 
+            secure: false, // Only set to true in production
         },
+        rolling: true, // Reset session maxAge on each request
         resave: false,
         saveUninitialized: false,
     })
-)
+);
 
+app.use('/api', userRoutes)
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
